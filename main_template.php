96c29853047058237088a902f5f83c2a8829ee5a
AlexBaranosky <alexander.baranosky@gmail.com>
2011-07-07 00:44:10 -0400

    fixed syntax in template stuff.  Added a .htaccess rule to reroute html requests to corresponding php requests

--- main_template.php
+++ main_template.php
@@ -1,10 +1,11 @@
 <?php
-$main_template = <<<END <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+$main_template = <<<END
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>{{title}}</title>
-<link href="css.css" rel="stylesheet" type="text/css" />
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <title>{{title}}</title>
+  <link href="css.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -44,9 +45,9 @@ $main_template = <<<END <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitiona
     </table>
 	
     <div class="content">
-	{{content}}	
+    	{{{content}}}	
     </div>
 </body>
 </html>
-END
+END;
 ?>

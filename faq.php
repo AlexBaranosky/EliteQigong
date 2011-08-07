81230f7d0c17367554922e94bd44c5569f856178
Alex Baranosky <alexander.baranosky@gmail.com>
2011-08-07 15:00:52 -0400

    oops, fix bug in page title

--- faq.php
+++ faq.php
@@ -3,7 +3,7 @@ include_once('mustache.php');
 include_once('main_template.php');
 
 $dataForTemplate = array(
-	'title' => strong,
+	'title' => FAQ,
 	'content' => <<<END
 <div class="welcome">FAQ</div>
     <br />

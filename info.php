b3289767e268d31b811fc430bb39213f0207ea6f
Alex Baranosky <alexander.baranosky@gmail.com>
2011-08-12 21:31:22 -0400

    cleaned info.php html

--- info.php
+++ info.php
@@ -5,10 +5,10 @@ include_once('main_template.php');
 $dataForTemplate = array(
 	'title' => "Ten Shaolin Laws",
 	'content' => <<<END
-    <p>The Ten Shaolin Laws are very important and  shouldn&rsquo;t be ignored by practitioners of our very special art.Â  The Laws were compiled and combined by my  teacher from wisdom passed down to him from previous masters and texts on  Shaolin Arts to make it a short and easy reference of important  principles. These principles have proven  to be important to adhere to in order to get not only the best results from  practice but to help students avoid problems as well.</p>
-    
-    <p>The Ten Shaolin Laws are  non-religious, and transcend all cultures and races, i.e. people of any culture  and race would agree that they promote values that are worthy and desirable.  Laws, in the Shaolin tradition, are not meant to be punitive or restrictive,  but as practical means to help followers achieve set aims and objectives; in  this case to help them attain the best possible results in practicing Shaolin  Chi Kung for health, joyful living, mind expansion and spiritual fulfillment.</p>
-      
+<p>The Ten Shaolin Laws are very important and  shouldn&rsquo;t be ignored by practitioners of our very special art.Â  The Laws were compiled and combined by my  teacher from wisdom passed down to him from previous masters and texts on  Shaolin Arts to make it a short and easy reference of important  principles. These principles have proven  to be important to adhere to in order to get not only the best results from  practice but to help students avoid problems as well.</p>
+
+<p>The Ten Shaolin Laws are  non-religious, and transcend all cultures and races, i.e. people of any culture  and race would agree that they promote values that are worthy and desirable.  Laws, in the Shaolin tradition, are not meant to be punitive or restrictive,  but as practical means to help followers achieve set aims and objectives; in  this case to help them attain the best possible results in practicing Shaolin  Chi Kung for health, joyful living, mind expansion and spiritual fulfillment.</p>
+  
 <p>There is no legal binding on the Ten Shaolin Laws; one cannot be prosecuted in a court of law if he breaks these laws. The binding is  moral and they are not forced upon the follower; the follower accepts them  because he chooses to because he believes they are helpful to him in his physical, emotional, mental and spiritual cultivation. If he breaks the laws, despite sufficient warnings, he may be asked to leave the Shaolin training, not as a punishment, but because the training is not suitable for him.</p>
 
 <ol start="1" type="1">
